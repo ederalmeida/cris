aa532523836d5459ea0a5257b4ce50a8a8386659
Eder Almeida <eder.almeida@gmail.com>
2021-11-04 22:48:56 -0300

    v1.2

--- janelas/janela_abertura.py
+++ janelas/janela_abertura.py
@@ -1,7 +1,7 @@
 # --- ROBO PARA CONECTAR E EXECUTAR TRANSACOES NO SAP --- #
 import PySimpleGUI as sg
 from imagens import imagens_base64 as imgs
-from robos import fagll03, fbl3n, kob1, sgpmr
+from robos import fagll03, fbl3n, kob1
 import sys
 
 def inicializacao():
@@ -9,31 +9,34 @@ def inicializacao():
     sg.theme('LightGrey1')
 
     coluna_esquerda = [[sg.Text('CRIS', size=(10,1), justification='center', font=("Helvetica", 50))],
-                       [sg.Text('Central de Robôs para Interação com o SAP', size=(40,1), justification='center', font=("Helvetica", 15))],
+                       [sg.Text('Central de Robôs para Interação com Sistemas', size=(20,2), justification='center', font=("Helvetica", 18))],
                        [sg.Text('')],
-                       [sg.Text('Contábil/Finanças', font=("Helvetica", 18), justification='left')],
-                       [sg.Text('─'  * 300, size=(25, 1), justification='left')],
-                       [sg.Text(' ' * 8),
-                        sg.Button('FBL3N', size=(10,1), key='-FBL3N-'),
-                        sg.Button('FAGLL03', size=(10,1), key='-FAGLL03-'),
-                        sg.Button('KOB1', size=(10,1), key='-KOB1-')],
+                       [sg.Frame('FINANÇAS/CONTÁBIL',
+                        [[sg.Button('FBL3N', size=(10,1), key='-FBL3N-'),
+                          sg.Button('FAGLL03', size=(10,1), key='-FAGLL03-'),
+                          sg.Button('KOB1', size=(10,1), key='-KOB1-')]])],
                        [sg.Text('')],
-                       [sg.Text('Técnicos', font=("Helvetica", 18), justification='left')],
-                       [sg.Text('─'  * 300, size=(25, 1), justification='left')],
-                       [sg.Text(' ' * 8),
-                        sg.Button('SGPMR', size=(10,1), key='-SGPMR-')],
+                       [sg.Frame('AMBIENTAL',
+                        [[sg.Button('IBAMA', size=(10,1), key='-IBAMA-'),
+                          sg.Button('INEA/RJ', size=(10,1), key='-INEARJ-'),
+                          sg.Button('CETESB/SP', size=(10,1), key='-CETESBSP-')],
+                         [sg.Button('SEMAD/GO', size=(10,1), key='-SEMADGO-'),
+                          sg.Button('SEMAD/MG', size=(10,1), key='-SEMADMG-'),
+                          sg.Button('IEMA/ES', size=(10,1), key='-IEMAES-')]])],
                        [sg.Text('')],
-                       [sg.Text('Ambiental', font=("Helvetica", 18), justification='left')],
-                       [sg.Text('─'  * 300, size=(25, 1), justification='left')],
-                       [sg.Text(' ' * 8),
-                        sg.Button('SIMA - SP', size=(10,1), key='-SIMA_SP-'),
-                        sg.Button('SEMAD - GO', size=(10,1), key='-SEMAD_GO-')],
-                       [sg.Text('')],
+                       [sg.Frame('REGULATÓRIO',
+                        [[sg.Button('BDIT', size=(10,1), key='-BDIT-'),
+                          sg.Button('SGT', size=(10,1), key='-SGT-'),
+                          sg.Button('BMP', size=(10,1), key='-BMP-')],
+                         [sg.Button('SIASE', size=(10,1), key='-SIASE-'),
+                          sg.Button('SINtegre', size=(10,1), key='-SINTEGRE-'),
+                          sg.Button('Dutonet', size=(10,1), key='-DUTONET-')]])],
+                       [sg.Text('')]
                       ]               
 
     coluna_direita = [[sg.Image(data=imgs.logo_x_base64, size=(150, 200), key='key1')]]
 
-    layout = [[sg.Column(coluna_esquerda), sg.VSeperator(),sg.Column(coluna_direita, element_justification='c')]]
+    layout = [[sg.Column(coluna_esquerda, element_justification='c'), sg.VSeperator(),sg.Column(coluna_direita, element_justification='c')]]
 
     janela= sg.Window('CRIS', layout, resizable=True)
 
@@ -50,9 +53,6 @@ def inicializacao():
         if event == '-KOB1-':
             janela.close()
             kob1.executa_robo()
-        if event == '-SGPMR-':
-            janela.close()
-            sgpmr.executa_robo()
         else:
             continue
                 
