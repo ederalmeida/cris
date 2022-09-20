7f1f7648a8555f23e32787a6cae9b3eaceefeda8
Eder Almeida <eder.almeida@gmail.com>
2022-09-20 18:48:19 -0300

    202209201848

--- janelas/janela_abertura_ate_v1.6.3.py
+++ janelas/janela_abertura_ate_v1.6.3.py
@@ -18,21 +18,37 @@ def exibir():
                           sg.Button('FAGLL03', size=(10,1), key='-FAGLL03-')],
                          [sg.Button('KOB1', size=(10,1), key='-KOB1-'),
                           sg.Button('AVD J1B1N', size=(10,1), key='-AVDJ1B1N-'),
-                          ]])],
+                          sg.Button('ONS AMSE', size=(10,1), key='-INADIMPLENCIA_AMSE-')]])],
                        [sg.Text('')],
-                       [sg.Frame('TESOURARIA',
-                        [[sg.Button('ONS AMSE', size=(10,1), key='-INADIMPLENCIA_AMSE-')]], size=(397, 50))],
+                       [sg.Frame('AMBIENTAL',
+                        [[sg.Button('IBMA', size=(10,1), key='-IBAMA-'),
+                         sg.Button('INEA/RJ', size=(10,1), key='-INEARJ-'),
+                         sg.Button('CETESB/SP', size=(10,1), key='-CETESB/SP-'),
+                         sg.Button('SEMAD/GO', size=(10,1), key='-SEMADGO-')],
+                         [sg.Button('SEMAD/MG', size=(10,1), key='-SEMADMG-'),
+                         sg.Button('IEMA/ES', size=(10,1), key='-IEMAES-')]])],
+                       [sg.Text('')],
+                       [sg.Frame('REGULATÓRIO',
+                        [[sg.Button('BDIT', size=(10,1), key='-BDIT-'),
+                         sg.Button('SGT', size=(10,1), key='-SGT-'),
+                         sg.Button('BMP', size=(10,1), key='-BMP-'),
+                         sg.Button('SIASE', size=(10,1), key='-SIASE-')],
+                         [sg.Button('SINtegre', size=(10,1), key='-SINTEGRE-'),
+                         sg.Button('Dutonet', size=(10,1), key='-DUTONET-')]])],
                        [sg.Text('')],
                        [sg.Text(versao.v, size=(10,1), justification='center', font=("Helvetica", 7))]
                       ]               
 
-    coluna_direita = [[sg.Image(data=imgs.logo_x_base64, size=(150, 170), key='key1')]]
+    coluna_direita = [[sg.Image(data=imgs.logo_x_base64, size=(150, 200), key='key1')]]
 
     layout = [[sg.Column(coluna_esquerda, element_justification='c'), sg.VSeperator(),sg.Column(coluna_direita, element_justification='c')]]
 
     janela= sg.Window('CRIS', layout, resizable=True)
 
     while True:
+
+        robos_nao_desenvolvidos = ['-IBAMA-', '-INEARJ-', '-CETESB/SP-', '-SEMADGO-', '-SEMADMG-', '-IEMAES-',
+                                   '-BDIT-', '-SGT-', '-BMP-', '-SIASE-', '-SINTEGRE-', '-DUTONET-']
 
         event, values = janela.read()
         if event in (sg.WIN_CLOSED, 'Exit'):
@@ -66,5 +82,8 @@ def exibir():
             janela.close()
             janela_inadimplencia_amse.exibir()
 
+        if event in robos_nao_desenvolvidos:
+            sg.popup('Sem autorização para acessar esse robô', title='Aviso')
+            continue
         else:
             continue
