2d1c604a302e23c9d2b00849f1705451f2b664a5
Éder Almeida <eder.almeida@gmail.com>
2022-09-22 08:22:43 -0300

    20220922

--- apoio/obter_arquivo_inadimplencia_amse.py
+++ apoio/obter_arquivo_inadimplencia_amse.py
@@ -19,7 +19,7 @@ def obter(arquivo, ignorar=True):
         pular_primeira_linha = 0
 
     for linha in range(1 + pular_primeira_linha, ultima_linha + 1):
-        for coluna in range(1, aba.max_column + 1):
+        for coluna in range(1, ultima_coluna + 1):
             dados_linha.append(aba.cell(row=linha, column=coluna).value)
         dados_documentos_inadimplentes.append(dados_linha)
         dados_linha = []
