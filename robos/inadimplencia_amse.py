c8c5add53887cda175dbe197d4cc28f26047e3f4
Éder Almeida <eder.almeida@gmail.com>
2022-09-26 10:49:02 -0300

    Update inadimplencia_amse.py

--- robos/inadimplencia_amse.py
+++ robos/inadimplencia_amse.py
@@ -15,19 +15,11 @@ def executar_robo(informacoes_janela_inadimplencia_amse):
         sistema_amse = amse.amse_site()
         sistema_amse.logon(informacoes_janela_inadimplencia_amse.get('login'), informacoes_janela_inadimplencia_amse.get('passwd'))
 
-<<<<<<< HEAD
     for documento in documentos_atrasados:
         # TODO - criar ação para caso não exista concessão na tabela de DE-PARA
         sistema_amse.inserir_documento_inadimplente(documento)
         glcdi.gravar_logo(informacoes_janela_inadimplencia_amse.get('arquivo_inadimplencia'), documento)
 
     sg.popup('cadastro de inadimplência efetuado com sucesso!')
-=======
-        for documento in documentos_atrasados:
-            # TODO - criar ação para caso não exista concessão na tabela de DE-PARA
-            sistema_amse.inserir_documento_inadimplente(documento)
->>>>>>> 939958924149951706357c726a49784e95d8f486
-
-        sg.popup('cadastro de inadimplência efetuado com sucesso!')
     
     jia.exibir()
