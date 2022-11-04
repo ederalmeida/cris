1ae3294ef2a8fe13ecd54c1926f12a0fcd72e608
Eder Almeida <eder.almeida@gmail.com>
2022-11-04 09:37:22 -0300

    v2.0.1.8

--- janelas/janela_update.py
+++ janelas/janela_update.py
@@ -13,8 +13,7 @@ def exibir():
     outputwin = [[sg.Output(size=(40,10))]]
 
     layout = [[sg.Frame('Progresso',layout= progressbar)],
-            [sg.Frame('Informações', layout = outputwin)],
-            [sg.Submit('Iniciar Atualização')]]
+            [sg.Frame('Informações', layout = outputwin)]]
 
     janela = sg.Window('Assistente de Atualização', layout)
 
@@ -23,25 +22,20 @@ def exibir():
 #TODO Refatorar essa parte
     while True:
         event, values = janela.read(timeout=10)
-        if event == 'Cancel'  or event is None:
-            break
-        elif event == 'Iniciar Atualização':
-            janela['Iniciar Atualização'].update(disabled=True)
-            i=0
-            print(lista_eventos[i])
-            up.download_arquivos()
-            progress_bar.UpdateBar(i + 1)
-            i += 1
-            print(lista_eventos[i])
-            up.atualizar_arquivos()
-            i += 1
-            print(lista_eventos[i])
-            up.substituir_hash()
-            i += 1
-            print(lista_eventos[i])
-            up.excluir_arquivos_tmp()
-            i += 1
-            print(lista_eventos[i])
-            janela['Iniciar Atualização'].update(disabled=False)
-            time.sleep(3)
-            break
+        i=0
+        print(lista_eventos[i])
+        up.download_arquivos()
+        progress_bar.UpdateBar(i + 1)
+        i += 1
+        print(lista_eventos[i])
+        up.atualizar_arquivos()
+        i += 1
+        print(lista_eventos[i])
+        up.substituir_hash()
+        i += 1
+        print(lista_eventos[i])
+        up.excluir_arquivos_tmp()
+        i += 1
+        print(lista_eventos[i])
+        time.sleep(3)
+        break
