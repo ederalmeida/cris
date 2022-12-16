133174420094644971b2aa043b7468563b7c7534
Éder Almeida <eder.almeida@gmail.com>
2022-12-16 17:01:28 -0300

    202211161701

--- robos/zmd_dirf.py
+++ robos/zmd_dirf.py
@@ -1,6 +1,7 @@
 import pyautogui as pag
 from classes import sapgui
 from janelas import janela_abertura as abertura
+from apoio import ober_relacao_contas as orc
 
 
 def executar_robo(informacoes_zmd_dirf):
@@ -8,7 +9,16 @@ def executar_robo(informacoes_zmd_dirf):
     sap = sapgui.SapGui()
     sap.logon()
 
-    
+    empresa = informacoes_zmd_dirf.get('company_code')
+    exercicio = informacoes_zmd_dirf.get('exercicio')
+    exercicio_corrente = informacoes_zmd_dirf.get('exercicio_corrente')
+    exercicio_anterior = informacoes_zmd_dirf.get('exercicio_anterior')
+    layout = informacoes_zmd_dirf.get('layout')
+    cpf = informacoes_zmd_dirf.get('ncpf')
+    caminho_cod_ctg_imp = informacoes_zmd_dirf.get('cod_imposto')
+
+    relacao_cod_cat_imposto = orc.obter_relacao_contas(caminho_cod_ctg_imp)
+
     # Abrindo a transação ZMD_DIRF
     sap.session.findById('wnd[0]').iconify()
     sap.session.findById('wnd[0]').maximize()
@@ -16,18 +26,31 @@ def executar_robo(informacoes_zmd_dirf):
     sap.session.findById('wnd[0]').sendVKey(0)
 
     # Cóndições de seleção
-    sap.session.findById("wnd[0]/usr/ctxtPCOMPANY").text = "ESUL"
-    sap.session.findById("wnd[0]/usr/txtPYEAR").text = "2021"
-    sap.session.findById("wnd[0]/usr/ctxtPWITHT-LOW").text = "E1"
-    
+    sap.session.findById("wnd[0]/usr/ctxtPCOMPANY").text = empresa
+    sap.session.findById("wnd[0]/usr/txtPYEAR").text = exercicio
+
+    if relacao_cod_cat_imposto != '':
+        sap.session.findById("wnd[0]/usr/btn%_PWITHT_%_APP_%-VALU_PUSH").press()
+        contador_numero_cod_cat_imposto_inseridos = 0
+        numero_do_print_cod_cat_imposto_inseridos = 0
+        screen_cod_ctg_imp = []
+        for cod_cat_imposto in relacao_cod_cat_imposto:
+            sap.session.findById("wnd[1]/tbar[0]/btn[13]").press()
+            sap.session.findById("wnd[1]/usr/tabsTAB_STRIP/tabpSIVA/ssubSCREEN_HEADER:SAPLALDB:3010/tblSAPLALDBSINGLE/ctxtRSCSEL_255-SLOW_I[1,0]").text = cod_cat_imposto
+            contador_numero_cod_cat_imposto_inseridos += 1
+            if (contador_numero_cod_cat_imposto_inseridos % 7 == 0) or contador_numero_cod_cat_imposto_inseridos == len(relacao_cod_cat_imposto):
+                            numero_do_print_cod_cat_imposto_inseridos += 1
+                            screen_cod_ctg_imp.append([numero_do_print_cod_cat_imposto_inseridos, pag.screenshot()])
+        sap.session.findById("wnd[1]/tbar[0]/btn[8]").press()
+
     # Parametros de Execução
     sap.session.findById("wnd[0]/usr/tabsTABSTRIP_TDATA/tabpMAIN_DATA/ssub%_SUBSCREEN_TDATA:ZJ_1BLFDI:1001/chkP_PUBLIC").selected = true
   
     # Dados Principais
-    sap.session.findById("wnd[0]/usr/tabsTABSTRIP_TDATA/tabpMAIN_DATA/ssub%_SUBSCREEN_TDATA:ZJ_1BLFDI:1001/txtCUR_YEAR").text = "2022"
-    sap.session.findById("wnd[0]/usr/tabsTABSTRIP_TDATA/tabpMAIN_DATA/ssub%_SUBSCREEN_TDATA:ZJ_1BLFDI:1001/txtPRE_YEAR").text = "2021"
-    sap.session.findById("wnd[0]/usr/tabsTABSTRIP_TDATA/tabpMAIN_DATA/ssub%_SUBSCREEN_TDATA:ZJ_1BLFDI:1001/txtLAYOUT").text = "XJFSFHB"
-    sap.session.findById("wnd[0]/usr/tabsTABSTRIP_TDATA/tabpMAIN_DATA/ssub%_SUBSCREEN_TDATA:ZJ_1BLFDI:1001/txtC_CPF").text = "623295109-34"
+    sap.session.findById("wnd[0]/usr/tabsTABSTRIP_TDATA/tabpMAIN_DATA/ssub%_SUBSCREEN_TDATA:ZJ_1BLFDI:1001/txtCUR_YEAR").text = exercicio_corrente
+    sap.session.findById("wnd[0]/usr/tabsTABSTRIP_TDATA/tabpMAIN_DATA/ssub%_SUBSCREEN_TDATA:ZJ_1BLFDI:1001/txtPRE_YEAR").text = exercicio_anterior
+    sap.session.findById("wnd[0]/usr/tabsTABSTRIP_TDATA/tabpMAIN_DATA/ssub%_SUBSCREEN_TDATA:ZJ_1BLFDI:1001/txtLAYOUT").text = layout
+    sap.session.findById("wnd[0]/usr/tabsTABSTRIP_TDATA/tabpMAIN_DATA/ssub%_SUBSCREEN_TDATA:ZJ_1BLFDI:1001/txtC_CPF").text = cpf
     sap.session.findById("wnd[0]/usr/tabsTABSTRIP_TDATA/tabpMAIN_DATA/ssub%_SUBSCREEN_TDATA:ZJ_1BLFDI:1001/txtCOMP_AM").text = ""
     sap.session.findById("wnd[0]/usr/tabsTABSTRIP_TDATA/tabpMAIN_DATA/ssub%_SUBSCREEN_TDATA:ZJ_1BLFDI:1001/chkP_PUBLIC").setFocus
 
@@ -41,12 +64,8 @@ def executar_robo(informacoes_zmd_dirf):
     sap.session.findById("wnd[0]/usr/tabsTABSTRIP_TDATA/tabpACC_DATA/ssub%_SUBSCREEN_TDATA:ZJ_1BLFDI:1002/txtR_PHONE").caretPosition = 8
 
     # Dados de Saida
-    sap.session.findById("wnd[0]/usr/tabsTABSTRIP_TDATA/tabpOUTPUT").select
-    sap.session.findById("wnd[0]/usr/tabsTABSTRIP_TDATA/tabpOUTPUT/ssub%_SUBSCREEN_TDATA:ZJ_1BLFDI:1003/radAPPLSERV").setFocus
     sap.session.findById("wnd[0]/usr/tabsTABSTRIP_TDATA/tabpOUTPUT/ssub%_SUBSCREEN_TDATA:ZJ_1BLFDI:1003/radAPPLSERV").select
     sap.session.findById("wnd[0]/usr/tabsTABSTRIP_TDATA/tabpOUTPUT/ssub%_SUBSCREEN_TDATA:ZJ_1BLFDI:1003/txtFNAMEAPS").text = "\\ELB3420\EP0\ESUL\FI\DIRF\2021\teste01.txt"
-    sap.session.findById("wnd[0]/usr/tabsTABSTRIP_TDATA/tabpOUTPUT/ssub%_SUBSCREEN_TDATA:ZJ_1BLFDI:1003/txtFNAMEAPS").setFocus
-    sap.session.findById("wnd[0]/usr/tabsTABSTRIP_TDATA/tabpOUTPUT/ssub%_SUBSCREEN_TDATA:ZJ_1BLFDI:1003/txtFNAMEAPS").caretPosition = 43
     
     
     sap.session.findById("wnd[0]/tbar[1]/btn[8]").press
