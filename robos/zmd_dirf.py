7a42490796f94a397814bd8a30c387489dab9c61
Eder Almeida <eder.almeida@gmail.com>
2022-12-19 03:06:26 -0300

    202212190306

--- robos/zmd_dirf.py
+++ robos/zmd_dirf.py
@@ -1,9 +1,14 @@
+import os
 import pyautogui as pag
-from classes import sapgui
-from janelas import janela_abertura as abertura
+import PySimpleGUI as sg
+import time
 from apoio import obter_relacao_contas as orc
 from apoio import verifica_cria_subpastas as vcs
+from apoio import verifica_janelas as vj
+from classes import sapgui
 from datetime import datetime
+from janelas import janela_abertura as abertura
+
 
 def executar_robo(informacoes_zmd_dirf):
     # Conectar ao SAP (seja por logon ou usando uma sessão já aberta)
@@ -69,7 +74,26 @@ def executar_robo(informacoes_zmd_dirf):
             screen[1].save(vcs.winapi_path(caminho_pasta_salvar_ipes) + '\\prints\\' + str(exercicio) + ' - ' + \
                     data_execucao + ' - código categoria impostos - ' + str(screen[0]) + '.jpg')
         sap.session.findById("wnd[1]/tbar[0]/btn[8]").press()
+     
+    # dados gerais
+    # Abrindo tela de status para confirmar ambiente
+    sap.session.findById("wnd[0]/mbar/menu[3]/menu[11]").select()
 
+    # colocando a tela de informação para o lado para não cobrir as informações geradas
+    time.sleep(1)
+    pag.press('alt')
+    pag.press('m')
+    for i in range(0, 11):
+        pag.press('right', presses=10)
+        time.sleep(0.5)
+    pag.click()
+    i = 0
+           
+    screen_dados_gerais = pag.screenshot()
+    screen_dados_gerais.save(vcs.winapi_path(caminho_pasta_salvar_ipes) + '\\prints\\' + str(exercicio) + ' - ' + \
+                    data_execucao + ' - dados gerais.jpg')
+    sap.session.findById("wnd[1]/tbar[0]/btn[0]").press()
+    
     # Dados do contador
     sap.session.findById("wnd[0]/usr/tabsTABSTRIP_TDATA/tabpACC_DATA").select()
     sap.session.findById("wnd[0]/usr/tabsTABSTRIP_TDATA/tabpACC_DATA/ssub%_SUBSCREEN_TDATA:ZJ_1BLFDI:1002/txtR_NAME").text = "Sandro Rodrigues da Silva"
@@ -77,22 +101,111 @@ def executar_robo(informacoes_zmd_dirf):
     sap.session.findById("wnd[0]/usr/tabsTABSTRIP_TDATA/tabpACC_DATA/ssub%_SUBSCREEN_TDATA:ZJ_1BLFDI:1002/txtR_DDD").text = "48"
     sap.session.findById("wnd[0]/usr/tabsTABSTRIP_TDATA/tabpACC_DATA/ssub%_SUBSCREEN_TDATA:ZJ_1BLFDI:1002/txtR_PHONE").text = "32317133"
     
+    # Abrindo tela de status para confirmar ambiente
+    sap.session.findById("wnd[0]/mbar/menu[3]/menu[11]").select()
+
+    # colocando a tela de informação para o lado para não cobrir as informações geradas
+    time.sleep(1)
+    pag.press('alt')
+    pag.press('m')
+    for i in range(0, 11):
+        pag.press('right', presses=10)
+        time.sleep(0.5)
+    pag.click()
+    i = 0
+    
     screen_dados_contador = pag.screenshot()
     screen_dados_contador.save(vcs.winapi_path(caminho_pasta_salvar_ipes) + '\\prints\\' + str(exercicio) + ' - ' + \
                     data_execucao + ' - dados contador.jpg')
+    sap.session.findById("wnd[1]/tbar[0]/btn[0]").press()
 
     # Dados de Saida
     sap.session.findById("wnd[0]/usr/tabsTABSTRIP_TDATA/tabpOUTPUT/").select()
     sap.session.findById("wnd[0]/usr/tabsTABSTRIP_TDATA/tabpOUTPUT/ssub%_SUBSCREEN_TDATA:ZJ_1BLFDI:1003/radAPPLSERV").select()
     sap.session.findById("wnd[0]/usr/tabsTABSTRIP_TDATA/tabpOUTPUT/ssub%_SUBSCREEN_TDATA:ZJ_1BLFDI:1003/txtFNAMEAPS").text = r"\\ELB3420\EP0\ESUL\FI\DIRF\2021\teste01.txt"
     
+    # Abrindo tela de status para confirmar ambiente
+    sap.session.findById("wnd[0]/mbar/menu[3]/menu[11]").select()
+
+    # colocando a tela de informação para o lado para não cobrir as informações geradas
+    time.sleep(1)
+    pag.press('alt')
+    pag.press('m')
+    for i in range(0, 11):
+        pag.press('right', presses=10)
+        time.sleep(0.5)
+    pag.click()
+    i = 0
+    
     screen_dados_saida = pag.screenshot()
     screen_dados_saida.save(vcs.winapi_path(caminho_pasta_salvar_ipes) + '\\prints\\' + str(exercicio) + ' - ' + \
                     data_execucao + ' - dados saida.jpg')
+    sap.session.findById("wnd[1]/tbar[0]/btn[0]").press()
+
     
-    
+    ####### 
+    # EXECUCANDO RELATORIO
     sap.session.findById("wnd[0]/tbar[1]/btn[8]").press()
-    sap.session.findById("wnd[0]/tbar[1]/btn[19]").press()
-    sap.session.findById("wnd[1]/usr/chkSFPOUTPAR-REQIMM").selected = True
-    sap.session.findById("wnd[1]/usr/chkSFPOUTPAR-REQIMM").setFocus()
-    sap.session.findById("wnd[1]/tbar[0]/btn[86]").press()
+    
+    # Abrindo tela de status para confirmar ambiente
+    time.sleep(2)
+    sap.session.findById("wnd[0]/mbar/menu[5]/menu[11]").select()
+    
+    # colocando a tela de informação para o lado para não cobrir as informações geradas
+    time.sleep(1)
+    pag.press('alt')
+    pag.press('m')
+    for i in range(0, 11):
+        pag.press('right', presses=10)
+        time.sleep(0.5)
+    pag.click()
+    i = 0
+    screen_resultado_execucao_inicio = pag.screenshot()
+    screen_resultado_execucao_inicio.save(vcs.winapi_path(caminho_pasta_salvar_ipes) + '\\prints\\' + str(exercicio) + ' - ' + \
+                    data_execucao + ' - resultado execução - início.jpg')
+    sap.session.findById("wnd[1]/tbar[0]/btn[0]").press()
+    time.sleep(1)
+    sap.session.findById("wnd[0]/usr/cntlGRID1/shellcont/shell").selectedRows = "0"
+    pag.keyDown('ctrl')
+    pag.keyDown('end')
+    pag.keyUp('end')
+    pag.keyUp('ctrl')
+    
+    # Abrindo tela de status para confirmar ambiente
+    sap.session.findById("wnd[0]/mbar/menu[5]/menu[11]").select()
+     # colocando a tela de informação para o lado para não cobrir as informações geradas
+    time.sleep(1)
+    pag.press('alt')
+    pag.press('m')
+    for i in range(0, 11):
+        pag.press('right', presses=10)
+        time.sleep(0.5)
+    pag.click()
+    i = 0
+    
+    screen_resultado_execucao_final = pag.screenshot()
+    screen_resultado_execucao_final.save(vcs.winapi_path(caminho_pasta_salvar_ipes) + '\\prints\\' + str(exercicio) + ' - ' + \
+                    data_execucao + ' - resultado execução - final.jpg')
+    time.sleep(1)
+    
+    #Exportanto para Excel
+    sap.session.findById('wnd[0]/mbar/menu[0]/menu[3]/menu[1]').select()
+    sap.session.findById('wnd[0]').sendVKey(0)
+    sap.session.findById('wnd[1]/usr/ctxtDY_PATH').text = caminho_pasta_salvar_ipes + '/relatorios'
+    sap.session.findById('wnd[1]/usr/ctxtDY_FILENAME').text = (str(exercicio) + ' - ' + data_execucao + ' - relatório gerado.xlsx')
+    sap.session.findById('wnd[1]').sendVKey(0)
+
+    # mapeando a janela ativa. Quando detectar que é o Excel, mata o processo.
+    janela_ativa = ''
+    while janela_ativa != 'EXCEL.EXE':
+        time.sleep(1)
+        janela_ativa = vj.verifica_janela_ativa()
+
+    # neste ponto, foi detectado que o Excel passou a ser a janela ativa, e com isso o processo é encerrado
+    os.system('TASKKILL /F /IM EXCEL.EXE')
+
+    # Espera um segundo, para que o processo seja finalizado
+    time.sleep(1)
+    
+    sap.session.findById("wnd[0]/tbar[0]/btn[3]").press()
+    sg.popup('Execução efetuada com sucesso')
